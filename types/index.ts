2ce41006db1ba852d4405faa02af53bcfcbdc972
Andrei Luca <1881266+iamandrewluca@users.noreply.github.com>
2024-11-28 13:22:25 +0200

    export all apis under namespaces

---
+++ types/index.ts
@@ -0,0 +1,6 @@
+export * as CheckoutApi from './checkout-api-v1';
+export * as ItineraryApi from './itinerary-api-v1';
+export * as LocationApi from './location-api-v1';
+export * as PaymentApi from './payment-transaction-api';
+export * as PlannerApi from './planner-api-v1';
+export * as TemplateApi from './template-api-v1';
